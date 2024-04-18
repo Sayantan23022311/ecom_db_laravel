9ec52359c1f1e88f3fac58b537e3eab7819963f2
rijumukherjee1234 <rijumukherjee506@gmail.com>
2024-04-18 16:34:47 +0530

    changes 1 st api

--- SERVER/index.js
+++ SERVER/index.js
@@ -2,15 +2,19 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3003;
 
 // Enable CORS for all origins
-app.use(cors()); //This is for connect frontend to backend code
+app.use(cors()); //This is for connect backend to frontend code
 
 // Define your API routes
 app.get('/api/ISLSHIELD', (req, res) => {
   res.json({ message: 'Joy Mohunbagan' });
 });
+app.get('/api/NEWAPI', (req, res) => {
+  res.json({ message: 'Ecoommerce' });
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
