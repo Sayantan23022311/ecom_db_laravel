b324abfdf3b68dde620e051df331b84d8843aeb2
rijumukherjee1234 <rijumukherjee506@gmail.com>
2025-01-25 17:40:43 +0530

    login done

--- src/models/usermodels.js
+++ src/models/usermodels.js
@@ -1,10 +1,12 @@
 const connection = require("../Config/db");
+const bcrypt = require('bcryptjs');
 
 // Function to find a user by username and password
-exports.findUserByCredentials = (USER_NAME, PASSWORD, callback) => {
+exports.findUserByCredentials = (USER_NAME, callback) => {
   const query =
-    "SELECT * FROM TBL_AUTHENTICATION WHERE USER_EMAIL = ? AND PASSWORD = ?";
-  connection.query(query, [USER_NAME, PASSWORD], callback);
+    "SELECT * FROM TBL_AUTHENTICATION WHERE USER_EMAIL = ?";
+  connection.query(query, [USER_NAME], callback);
+ 
 };
 
 
